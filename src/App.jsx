c9be64df304d515f04edca8c4508326e9616ffdf
JesWWW <jessicasw58467278@gmail.com>
2025-03-24 12:41:03 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>R13921A22 Yun-Jing Website</h1>
+        <p>I am a graduate student and currently studying in Electrical Engineering.</p>
+      </div>
+      <h1>Feel free to leave a message</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
